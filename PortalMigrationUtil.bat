89ed7aa2277ca2d826e0ac90b536e025d5ae80e8
Panchal <panra16@ca.com>
2018-04-03 13:33:52 +0800

    Updated PortalMigrationUtil.bat file

--- PortalMigrationUtil.bat
+++ PortalMigrationUtil.bat
@@ -1,5 +1,5 @@
 @echo off
 @REM Set file path for logging the output of this script
-SET logging.file=C:/temp/portalapimigration.log
-
+@REM SET logging.file=C:/temp/portalapimigration.log
+SET logging.file=portalapimigration.log
 java -jar target/PortalMigrationUtil-1.0.jar %*
